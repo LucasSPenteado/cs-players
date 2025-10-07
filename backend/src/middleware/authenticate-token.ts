19ec730f46d8603c527fde6f177af438aa85a7b6
Lucas Silva Penteado <lucaspenteado72@gmail.com>
2025-10-07 14:08:54 +0100

    feat: now middleware authenticate refresh token too

--- backend/src/middleware/authenticate-token.ts
+++ backend/src/middleware/authenticate-token.ts
@@ -1,5 +1,6 @@
 import { env } from "@/env/index.js";
 import { AuthenticationError } from "@/errors/authentication-error.js";
+import { BadRequestError } from "@/errors/bad-request-error.js";
 import { DataBaseError } from "@/errors/database-error.js";
 import { prisma } from "@/lib/prisma.js";
 import type { NextFunction, Request, Response } from "express";
@@ -19,9 +20,27 @@ export const authenticateToken = async (
       email: string;
     };
 
+    const cookies = req.cookies;
+    if (!cookies?.jwt) throw new BadRequestError("");
+
+    const refreshToken = req.cookies.jwt;
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
+
+    if (!refreshToken) {
+      throw new AuthenticationError("");
+    }
+
+    jwt.verify(refreshToken, env.REFRESH_TOKEN_SECRET);
+
     const user = await prisma.user.findUnique({
       where: { email: email },
-      select: { email: true, firstName: true, lastName: true },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        createdAt: true,
+      },
     });
     if (!user) {
       throw new DataBaseError("");
@@ -30,6 +49,9 @@ export const authenticateToken = async (
     req.user = user;
     next();
   } catch (error) {
+    if (error instanceof BadRequestError) {
+      return next(new BadRequestError("Bad request of cookies"));
+    }
     if (error instanceof DataBaseError) {
       return next(new DataBaseError("Error fetching user from database"));
     }
